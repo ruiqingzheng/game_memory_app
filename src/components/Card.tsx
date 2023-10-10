6b02b7e906630ca4a23a009f6feddc34ea0ad685
ruiqingzheng <552359135@qq.com>
2023-10-10 14:09:55 +0800

    ✨ feat(check match): is matched and style

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,28 +1,48 @@
 import { CardStatus } from '../types.d'
 import { cardStyling } from '../globalStyles'
 import styled from 'styled-components'
+import { useState } from 'react'
 
 interface IProps {
   status: CardStatus // faceup or facedown
   symbol: string
   matched: boolean // matched : card exam result
+  gameStarted: boolean
   onClickCard: () => void
+  isCurrentPairing: boolean
 }
 
-export const Card = ({ status, symbol, onClickCard, matched }: IProps) => {
+export const Card = ({ status, symbol, onClickCard, matched, gameStarted, isCurrentPairing }: IProps) => {
+  const [isFlipping, setIsFlipping] = useState(false)
+
+  const _onClick = () => {
+    if (!gameStarted) return
+    onClickCard()
+    flipCardAnimate()
+  }
+
+  const flipCardAnimate = () => {
+    if (matched) return
+    setIsFlipping(true)
+    const x = setTimeout(() => {
+      setIsFlipping(false)
+      if (x) clearTimeout(x)
+    }, 500)
+  }
+
   return (
-    <S.Card onClick={onClickCard} matched={matched} className="grid place-content-center">
+    <S.Card onClick={_onClick} matched={matched} isFlipping={isFlipping} isCurrentPairing={isCurrentPairing} className="grid place-content-center bg-yellow-400">
       {status === CardStatus.faceup ? symbol : ''}
     </S.Card>
   )
 }
 
 const S = {
-  Card: styled.div<{ matched: boolean }>`
+  Card: styled.div<{ matched: boolean; isFlipping: boolean; isCurrentPairing: boolean }>`
     ${cardStyling}
-    border-color: ${(p) => (p.matched ? '#FF9A00' : 'violet')};
-    background-color: ${(p) => (p.matched ? '#AB0097' : '#ffCFCF')};
-    animation: ${(p) => (p.matched ? 'tada' : 'flipInY')};
-    animation-duration: 1s;
+    border-color: ${(p) => (p.matched || p.isFlipping ? '#FF9A00' : 'violet')};
+    background-color: ${(p) => (p.matched || (p.isFlipping && !p.isCurrentPairing) ? '#AB0097' : p.isCurrentPairing ? 'rgb(251, 191, 36)' : '#ffCFCF')};
+    animation: ${(p) => (p.isFlipping ? 'flipInY' : p.matched ? 'tada' : 'swing')};
+    animation-duration: 0.5s;
   `,
 }
--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -15,11 +15,13 @@ enum ActionType {
   setAllFacedown = 'setAllFacedown',
   setAllFaceUp = 'setAllFaceUp',
   setGameStarted = 'setGameStarted',
+  setCurrentPairingCard = 'setCurrentPairingCard',
 }
 
 type CardStatePayload = Omit<ICardState, symbol>
+type PairingCardPayload = ICardState | null
 type IsGameStartedPayload = boolean
-type ActionPayload = CardStatePayload | IsGameStartedPayload
+type ActionPayload = CardStatePayload | IsGameStartedPayload | PairingCardPayload
 interface Action {
   type: ActionType
   payload?: ActionPayload
@@ -28,6 +30,7 @@ interface Action {
 interface GameState {
   cardsState: ICardState[]
   started: boolean
+  currentPairingCard: ICardState | null
 }
 
 const initCards = (symbols: string[]): ICardState[] => {
@@ -101,6 +104,11 @@ function gameReducer(state: GameState, action: Action) {
       }, 3000)
       return _state
     }
+
+    case ActionType.setCurrentPairingCard: {
+      state.currentPairingCard = action.payload as PairingCardPayload
+      return { ...state }
+    }
     default:
       return state
   }
@@ -108,6 +116,7 @@ function gameReducer(state: GameState, action: Action) {
 
 const initState: GameState = {
   cardsState: [],
+  currentPairingCard: null,
   started: false,
 }
 
@@ -116,26 +125,57 @@ const Game = ({ symbols }: IProps) => {
     return { ...initState, cardsState: initCards(symbols) }
   })
 
+  // current pairing card
+  // const [currentPairingCard, setCurrentPairingCard] = useState<ICardState | null>(null)
+
   const startTimer = useRef<number | null>(null)
 
-  const toggleCard = (card: ICardState) => {
-    const _card: ICardState = { ...card, status: card.status === CardStatus.facedown ? CardStatus.faceup : CardStatus.facedown }
-    dispatch({ type: ActionType.setStatus, payload: _card })
+  const flipCard = (card: ICardState) => {
+    // if (card.matched) return
+    // const _card: ICardState = { ...card, status: card.status === CardStatus.facedown ? CardStatus.faceup : CardStatus.facedown }
+    dispatch({ type: ActionType.setStatus, payload: { ...card, status: CardStatus.faceup } })
   }
 
-  const toggleMatched = (card: ICardState) => {
-    const _card: ICardState = { ...card, matched: !card.matched }
-    dispatch({ type: ActionType.setMatched, payload: _card })
-  }
+  // const toggleMatched = (card: ICardState) => {
+  //   const _card: ICardState = { ...card, matched: !card.matched }
+  //   dispatch({ type: ActionType.setMatched, payload: _card })
+  // }
 
   const onClickCard = (card: ICardState) => {
     if (!gameState.started) return
-    toggleCard(card)
-    toggleMatched(card)
+    flipCard(card)
+    checkMatched(card)
+    // toggleMatched(card)
+  }
+
+  const checkMatched = (card: ICardState) => {
+    if (card.matched) return
+
+    if (gameState.currentPairingCard && card.id === gameState.currentPairingCard.id) return
+
+    // set current pairing card
+    if (gameState.currentPairingCard === null && !card.matched) {
+      dispatch({ type: ActionType.setCurrentPairingCard, payload: card })
+      dispatch({ type: ActionType.setStatus, payload: { ...card, status: CardStatus.faceup } })
+    }
+
+    // check card if match current pairing card
+    if (gameState.currentPairingCard && !card.matched) {
+      if (gameState.currentPairingCard.symbol === card.symbol) {
+        dispatch({ type: ActionType.setMatched, payload: { ...card, matched: true } })
+        dispatch({ type: ActionType.setMatched, payload: { ...gameState.currentPairingCard, matched: true } })
+        dispatch({ type: ActionType.setCurrentPairingCard, payload: null })
+      } else {
+        setTimeout(() => {
+          dispatch({ type: ActionType.setStatus, payload: { ...card, status: CardStatus.facedown } })
+        }, 500)
+      }
+    }
   }
 
   const restartGame = () => {
     dispatch({ type: ActionType.setGameStarted, payload: false })
+    dispatch({ type: ActionType.setCurrentPairingCard, payload: null })
     dispatch({ type: ActionType.shuffle })
 
     startGame()
@@ -146,6 +186,7 @@ const Game = ({ symbols }: IProps) => {
       dispatch({ type: ActionType.setAllFaceUp })
       startTimer.current = setTimeout(() => {
         dispatch({ type: ActionType.setAllFacedown })
+        dispatch({ type: ActionType.setCurrentPairingCard, payload: null })
         dispatch({ type: ActionType.setGameStarted, payload: true })
         if (startTimer.current) clearTimeout(startTimer.current)
       }, 3000)
@@ -155,6 +196,10 @@ const Game = ({ symbols }: IProps) => {
     }
 
     _fn()
+  }
+
+  const isCurrentPairing = (card: ICardState) => {
+    return gameState.currentPairingCard !== null && card.id === gameState.currentPairingCard.id
   }
 
   useEffect(() => {
@@ -168,7 +213,7 @@ const Game = ({ symbols }: IProps) => {
     <S.Container>
       <div className="grid grid-cols-5 ">
         {gameState.cardsState.map((cardData) => (
-          <Card key={cardData.id} status={cardData.status} symbol={cardData.symbol} onClickCard={() => onClickCard(cardData)} matched={cardData.matched}></Card>
+          <Card key={cardData.id} isCurrentPairing={isCurrentPairing(cardData)} status={cardData.status} symbol={cardData.symbol} gameStarted={gameState.started} onClickCard={() => onClickCard(cardData)} matched={cardData.matched}></Card>
         ))}
       </div>
       <S.StartButton onClick={restartGame} className="mt-5 rounded-lg px-5 py-1 tracking-widest bg-pink-500 hover:bg-pink-400 active:bg-pink-300 text-white font-bold text-[20px] font-serif italic">
